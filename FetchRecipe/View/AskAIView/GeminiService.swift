14620ee8416d0e32ba4a6170b166a01a7d988267
nuttysunday <shivamghodke10000@gmail.com>
2026-02-03 22:57:10 -0500

    chore: updated api key and gemin api endpoint
    Remove Config.swift from repository
    
    Ignore Config.swift

--- FetchRecipe/View/AskAIView/GeminiService.swift
+++ FetchRecipe/View/AskAIView/GeminiService.swift
@@ -6,24 +6,19 @@
 
 import Foundation
 
-
 class GeminiService {
     
     private var apiKey: String?
     
     func generateAIResponse(userQuestion: String, recipe: Recipe) async throws -> String {
         
-        if let apiKey = Bundle.main.object(forInfoDictionaryKey: "Gemini_api_key") as? String {
-            self.apiKey = apiKey
-        } else {
-            print("API Key not found!")
-        }
-        
+        self.apiKey = Config.geminiAPIKey
+
         guard let apiKey = self.apiKey else {
             throw NSError(domain: "GeminiService", code: 1, userInfo: [NSLocalizedDescriptionKey: "API key is missing."])
         }
 
-        let url = URL(string: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=\(apiKey)")!
+        let url = URL(string: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=\(apiKey)")!
         
         let requestPayload = GeminiRequest(
             contents: [
